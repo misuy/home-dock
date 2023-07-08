5eec129912e579d74b20ab200f603b132ce4c982
misuy <mperedriy@bk.ru>
2023-07-09 00:19:51 +0300

    storage stuff

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,30 @@ mod storage;
 #[macro_use] extern crate rocket;
 
 
-#[get("/read_file/<path..>")]
-fn read_file(storage: &rocket::State<storage::Storage>, path: std::path::PathBuf) -> rocket::serde::json::Json<storage::File>
+#[post("/read_file", data = "<path>")]
+fn read_file(storage: &rocket::State<storage::Storage>, path: rocket::serde::json::Json<storage::EntryPath>) -> Result<rocket::serde::json::Json<storage::File>, std::io::Error>
 {
-    return rocket::serde::json::Json(storage.read_file(path).unwrap());
+    let mut file = storage::File::new(path.into_inner());
+    return match file.read(storage)
+    {
+        Ok(()) => Ok(rocket::serde::json::Json(file)),
+        Err(err) => Err(err),
+    }
 }
 
 
-#[post("/write_file/<path..>")]
-fn write_file(storage: &rocket::State<storage::Storage>, path: std::path::PathBuf) -> ()
+#[post("/write_file", data = "<file>")]
+fn write_file(storage: &rocket::State<storage::Storage>, file: rocket::serde::json::Json<storage::File>) -> ()
 {
-    
+    file.write(storage);
 }
+
+
 
 
 #[launch]
 fn launch() -> rocket::Rocket<rocket::Build>
 {
     let storage = storage::Storage::init().unwrap();
-    rocket::build().manage(storage).mount("/home_dock_api", routes![read_file])
+    rocket::build().manage(storage).mount("/home_dock_api", routes![read_file, write_file])
 }
--- src/storage.rs
+++ src/storage.rs
@@ -1,3 +1,5 @@
+use std::fs::ReadDir;
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum StorageError
 {
@@ -9,28 +11,124 @@ pub struct Storage
     path_buf: std::path::PathBuf,
 }
 
+
+#[derive(rocket::serde::Serialize, rocket::serde::Deserialize)]
+pub enum EntryType
+{
+    File,
+    Dir,
+}
+
+#[derive(rocket::serde::Serialize, rocket::serde::Deserialize)]
+pub struct EntryPath
+{
+    path: std::path::PathBuf,
+    entry_type: EntryType,
+}
+
+
+#[derive(rocket::serde::Serialize, rocket::serde::Deserialize)]
+pub enum FileContent {
+    Content(Vec<u8>),
+    NULL,
+}
+
 #[derive(rocket::serde::Serialize, rocket::serde::Deserialize)]
 pub struct File
 {
-    content: Vec<u8>,
+    path: EntryPath,
+    content: FileContent,
 }
+
+
+impl File
+{
+    pub fn new(path: EntryPath) -> File
+    {
+        return File { path: path, content: FileContent::NULL };
+    }
+
+    pub fn read(&mut self, storage: &Storage) -> Result<(), std::io::Error>
+    {
+        return match storage.read(&self.path.path) {
+            Ok(content) =>
+            {
+                self.content = FileContent::Content(content);
+                Ok(())
+            },
+            Err(err) => Err(err),
+        }
+    }
+
+    pub fn write(&self, storage: &Storage) -> Result<bool, std::io::Error>
+    {
+        let content = &self.content;
+        return match content
+        {
+            FileContent::Content(content) => 
+            {
+                match storage.write(&self.path.path, content)
+                {
+                    Ok(()) => Ok(true),
+                    Err(err) => Err(err),
+                }
+            }
+            FileContent::NULL => Ok(false),
+        }
+    }
+}
+
+
+#[derive(rocket::serde::Serialize, rocket::serde::Deserialize)]
+pub enum DirEntries {
+    Content(Vec<EntryPath>),
+    NULL,
+}
+
+#[derive(rocket::serde::Serialize, rocket::serde::Deserialize)]
+pub struct Dir
+{
+    path: EntryPath,
+    entries: DirEntries,
+}
+
 
 impl Storage
 {
-    pub fn init() -> Result<Storage, StorageError>
+    pub fn init() -> Result<Storage, std::io::Error>
     {
         let storage_path_string = std::env::var("HOME_DOCK_STORAGE_PATH").unwrap();
         let storage_path = std::path::Path::new(&storage_path_string);
-        if !storage_path.exists() { if std::fs::create_dir(storage_path).is_err() { return Err(StorageError::CantCreateStorageDir); } }
-        return Ok( Storage { path_buf: storage_path.to_path_buf() } );
+        return match storage_path.exists()
+        {
+            true => Ok( Storage { path_buf: storage_path.to_path_buf() } ),
+            false => match std::fs::create_dir(storage_path) 
+            {
+                Ok(i) => Ok( Storage { path_buf: storage_path.to_path_buf() } ),
+                Err(err) => Err(err),
+            }
+        }
     }
 
-    pub fn read_file(&self, storage_file_path: std::path::PathBuf) -> Result<File, std::io::Error>
+    pub fn convert_storage_path(&self, storage_path: &std::path::PathBuf) -> std::path::PathBuf
     {
         let mut file_path = self.path_buf.clone();
-        file_path.push(storage_file_path);
-        let read_result = std::fs::read(file_path);
-        if read_result.is_err() { return Err(read_result.unwrap_err()) };
-        return Ok(File { content: read_result.unwrap() });
+        file_path.push(storage_path);
+        return file_path;
+    }
+
+    pub fn read(&self, storage_path: &std::path::PathBuf) -> Result<Vec<u8>, std::io::Error>
+    {
+        return std::fs::read(self.convert_storage_path(storage_path));
+    }
+
+    pub fn write(&self, storage_path: &std::path::PathBuf, content: &Vec<u8>) -> Result<(), std::io::Error>
+    {
+        return std::fs::write(self.convert_storage_path(storage_path), content);
+    }
+
+    pub fn read_dir(&self, storage_path: &std::path::PathBuf) -> Result<ReadDir, std::io::Error>
+    {
+        return std::fs::read_dir(self.convert_storage_path(storage_path));
     }
 }
